936f278362082048c1d6f6662d56646c92850edf
Toni Sanmartin <toni.sanmartin@simplepractice.com>
2026-01-13 14:10:25 -0600

    test: add more assertions to sample spec

--- spec/features/sample_spec.rb
+++ spec/features/sample_spec.rb
@@ -5,9 +5,11 @@ describe 'Sample automation test' do
 
   it 'does many things at once' do
     @client.name = 'Mike'
+    find(:xpath, "//div[text()='Save']").click
     expect(@client.name).to eq('Mike')
     expect(1).to eq(1)
     expect(2).to eq(2)
     expect(3).to eq(3)
+    expect(4).to eq(4)
   end
 end
